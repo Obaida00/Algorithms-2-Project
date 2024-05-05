e220c32196473f8ee850956683d5410d2775b5e5
Doha-Alarbeed <168964019+Doha-Alarbeed@users.noreply.github.com>
2024-05-05 13:56:15 +0300

    fix wrong path

--- App 2/src/App.java
+++ App 2/src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        FileHandler fileHandler = new FileHandler("src\\data.txt", "src\\output.txt");
+        FileHandler fileHandler = new FileHandler("data.txt", "output.txt");
 
         Tree tree = fileHandler.loadTree();
         fileHandler.saveTree(tree);
