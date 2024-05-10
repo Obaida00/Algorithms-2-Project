d407ddce704e0c7c0d7efaabaafda1523f4a4960
talalcodes55 <talalalghazal55@gmail.com>
2024-05-10 21:57:34 +0300

    added the Front End classes for UI and added the required libs

---
+++ App/src/FrontEnd/Controller.java
@@ -0,0 +1,11 @@
+package FrontEnd;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+    
+    public void addBtneventTest(ActionEvent e){
+        System.out.println("This button works!");
+    }
+
+}
